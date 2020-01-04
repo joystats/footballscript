8d7f53c1ddddbb979940a388f4c3c96a0bf5d4a6
k <joystats@yahoo.com>
2020-01-04 11:20:34 +0700

    edit

--- src/pages/Front.js
+++ src/pages/Front.js
@@ -65,65 +65,10 @@ class Front extends Component{
 
 				<h4 className="pridi marginTop marginBottom">
 					ข่าวสารทั้งหมด
-				</h4>	
-                <div className="row">
-					<div className="col-md-12">
-
-<ul className="list-unstyled">
-	<li className="media">
-		<img src="./images/a1.jpg" className="mr-3" alt=""/>
-		<div className="media-body">vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-		</div>
-	</li>
-	<li className="media">
-		<img src="./images/a2.jpg" className="mr-3" alt=""/>
-		<div className="media-body">
-			Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-		</div>
-	</li>
-	<li className="media">
-		<img src="./images/a3.jpg" className="mr-3" alt=""/>
-		<div className="media-body">
-			Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-		</div>
-	</li>
-	<li className="media">
-		<img src="./images/a4.jpg" className="mr-3" alt=""/>
-		<div className="media-body">
-			Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-		</div>
-	</li>
-	<li className="media">
-		<img src="./images/a1.jpg" className="mr-3" alt=""/>
-		<div className="media-body">vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-		</div>
-	</li>
-	<li className="media">
-		<img src="./images/a2.jpg" className="mr-3" alt=""/>
-		<div className="media-body">
-			Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-		</div>
-	</li>
-	<li className="media">
-		<img src="./images/a3.jpg" className="mr-3" alt=""/>
-		<div className="media-body">
-			Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-		</div>
-	</li>
-	<li className="media">
-		<img src="./images/a4.jpg" className="mr-3" alt=""/>
-		<div className="media-body">
-			Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-		</div>
-	</li>
-</ul>
-<p className="marginTop">
-	<button className="right">หน้าถัดไป &rarr;</button>
-</p>
-						
-
-					</div>
-				</div>
+				</h4>
+				<p className="marginTop">
+					<button className="right">หน้าถัดไป &rarr;</button>
+				</p>
             </div>
 		);
 	}
